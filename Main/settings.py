5bf9ac81b26a97b3df3ab1d8e41b3013b69490ef
mykhailokukol <mykhailokukol@gmail.com>
2021-08-12 22:18:32 +0300

    add header

--- Main/settings.py
+++ Main/settings.py
@@ -111,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
@@ -126,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    STATICFILES_DIR,
+]
