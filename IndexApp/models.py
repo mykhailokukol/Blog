cf35b76581f5d9b95bd6c3673395112505a6481b
mykhailokukol <mykhailokukol@gmail.com>
2021-08-13 18:45:47 +0300

    siple profile page

--- IndexApp/models.py
+++ IndexApp/models.py
@@ -5,10 +5,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	age = models.SmallIntegerField(default=18)
+	location = models.CharField(max_length=32, null=True)
+	birth_date = models.DateField(null=True)
 	activity = models.CharField(max_length=128, null=True)
 	about_text = models.TextField(null=True)
 	created = models.DateTimeField(auto_now_add=True, null=True)
 
 	def __str__(self):
-		return ('%s %s' % (self.first_name, self.last_name))
+		return ('%s %s' % (self.user.first_name, self.user.last_name))
--- IndexApp/views.py
+++ IndexApp/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
+from datetime import date
+from django.contrib.auth.decorators import login_required, user_passes_test
+from IndexApp import models
 
 # Create your views here.
 def index(request):
 
 	return render(request, 'IndexApp/index.html', {
 
+		})
+
+@login_required
+def profile(request):
+	user = request.user
+	profiles_list = models.Profile.objects.all()
+	profile = profiles_list.get(user=user.id)
+	# Начало формулы расчета возраста
+	age = date.today().year - profile.birth_date.year - (
+		(date.today().month, date.today().day) < (
+			profile.birth_date.month, profile.birth_date.day
+			)
+		)
+	# Конец формулы расчета возраста
+
+	return render(request, 'IndexApp/profile.html', {
+			'profile': profile,
+			'user_age': age,
 		})
