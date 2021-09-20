bd3c56f304ce4b49c74a9a033c6e8cb893e38c75
mykhailokukol <mykhailokukol@gmail.com>
2021-09-21 01:34:25 +0300

    add chat messages editing

--- Chat/urls.py
+++ Chat/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    path('', views.ChatView.as_view(), name='chat_page'),
+    path('', login_required(views.ChatView.as_view()), name='chat_page'),
+    path('<int:pk>/', login_required(views.MessageEditView.as_view()), name='edit_msg_page'),
 ]
--- Chat/views.py
+++ Chat/views.py
@@ -15,11 +15,37 @@ class ChatView(View):
         })
 
     def post(self, request):
+
         form = forms.MessageForm(request.POST, request.FILES)
         if form.is_valid():
             text = form.cleaned_data['text']
             img = form.cleaned_data['image']
             message = models.Message.objects.create(author=request.user, text=text, image=img)
+            return redirect('/chat#id_text')
+        else:
+            return redirect('/chat#id_text')
+
+class MessageEditView(View):
+
+    def get(self, request, pk):
+        edit_form = forms.MessageForm()
+        messages = models.Message.objects.all().order_by('sent')
+        return render(request, 'Chat/chat.html', {
+            'edit_form': edit_form,
+            'messages': messages,
+            'pk': pk,
+        })
+
+    def post(self, request, pk):
+
+        edit_form = forms.MessageForm(request.POST, request.FILES)
+        if edit_form.is_valid():
+            text = edit_form.cleaned_data['text']
+            img = edit_form.cleaned_data['image']
+            message = models.Message.objects.get(pk=pk)
+            message.text = text
+            message.image = img
+            message.save()
             return redirect('/chat#id_text')
         else:
             return redirect('/chat#id_text')
